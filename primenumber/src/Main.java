97af1349c228fc3f033ea2b16ad9583bdb48830f
Ranjith0702 <116152493+Ranjith0702@users.noreply.github.com>
2022-10-27 16:40:00 +0530

    Add files via upload

--- primenumber/src/Main.java
+++ primenumber/src/Main.java
@@ -5,25 +5,33 @@ import java.util.Scanner;
 class Primenumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of counts: ");
+        int n = sc.nextInt();
+        int a[]= new int[n];
         System.out.println("Enter a number: ");
-        int a = sc.nextInt();
-        int b = a / 2;
-        int c = 0;
-        if (a == 0 || a == 1) {
-            System.out.println("It is not a prime number");
-        } else {
-            for (int i = 2; i <= b; i++) {
-                if (a % i == 0) {
-                    System.out.println("It is not a prime number");
-                    c = 1;
-                    break;
+        for (int i : a) {
+            a[i] = sc.nextInt();
+            int b = a[i] / 2;
+            int c = 0;
+
+            if (a[i] == 0 || a[i] == 1) {
+                System.out.println("It is not a prime number");
+            } else {
+                for (int j = 2; j <= b; j++) {
+                    if (a[i] % j == 0) {
+                        System.out.println("It is not a prime number");
+                        c = 1;
+                        break;
+                    }
+                }
+
+                if (c == 0) {
+                    System.out.println("It is a prime number");
+
                 }
             }
-
-            if (c == 0) {
-                System.out.println("It is a prime number");
-
-            }
         }
+
+
     }
 }
